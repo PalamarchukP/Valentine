91508cdedddba98c9093ae0ce6cc415cce12604a
Kirill Levchuk <kirya.levch@mail.ru>
2026-02-12 01:39:01 +0200

    love u

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// // Получаем элементы кнопок и иконки сердечка
-// const petButton1 = document.getElementById("petButton1");
-// const petButton2 = document.getElementById("petButton2");
-// const heartIcon = document.getElementById("heartIcon");
 
-// // Функция для показа и анимации сердечка
-// function showHeartIcon() {
-//   heartIcon.style.display = "inline-block"; // Делаем иконку видимой
-//   heartIcon.classList.remove("animate-heart"); // Убираем старую анимацию
-//   void heartIcon.offsetWidth; // Принудительное обновление стилей для анимации
-//   heartIcon.classList.add("animate-heart"); // Запускаем анимацию
-// }
-
-// // Добавляем обработчик событий на кнопки "погладить"
-// petButton1.addEventListener("click", showHeartIcon);
-// petButton2.addEventListener("click", showHeartIcon);
